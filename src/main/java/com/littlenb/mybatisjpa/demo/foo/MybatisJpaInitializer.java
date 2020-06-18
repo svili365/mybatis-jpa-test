27a29e7795fb92f41fe68821f3abc276e6725663
sway.li <swayli365@gmail.com>
2020-06-18 23:34:07 +0800

    test for v2.5.1

--- src/main/java/com/littlenb/mybatisjpa/demo/foo/MybatisJpaInitializer.java
+++ src/main/java/com/littlenb/mybatisjpa/demo/foo/MybatisJpaInitializer.java
@@ -1,42 +1,41 @@
 package com.littlenb.mybatisjpa.demo.foo;
 
 
+import com.littlenb.mybatisjpa.keygen.DefaultIdGenerator;
 import com.littlenb.mybatisjpa.keygen.IdentityKeyGenerator;
 import com.littlenb.mybatisjpa.statement.AnnotationStatementRegistry;
 import com.littlenb.mybatisjpa.support.AnnotationStatementScanner;
 import com.littlenb.mybatisjpa.support.Constant;
 import com.littlenb.mybatisjpa.support.MybatisJapConfiguration;
 import com.littlenb.mybatisjpa.util.NamingPolicy;
-import javax.annotation.PostConstruct;
 import org.apache.ibatis.executor.keygen.KeyGenerator;
-import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author sway.li
  **/
-@Component
+@Configuration
 public class MybatisJpaInitializer {
 
-  @Autowired
-  private SqlSessionFactory sqlSessionFactory;
-
-  @PostConstruct
-  public void init() {
+  @Bean
+  public AnnotationStatementScanner annotationStatementScanner(SqlSessionFactory sqlSessionFactory) {
+    // 全局配置
     MybatisJapConfiguration conf = MybatisJapConfiguration.getInstance();
     conf.setTableNamingStrategy(NamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
     conf.setColumnNamingStrategy(NamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
 
-    Configuration configuration = sqlSessionFactory.getConfiguration();
-    KeyGenerator keyGenerator = new IdentityKeyGenerator(new MyIdGenerator());
+    org.apache.ibatis.session.Configuration configuration = sqlSessionFactory.getConfiguration();
+    KeyGenerator keyGenerator = new IdentityKeyGenerator(DefaultIdGenerator.newInstance());
     configuration.addKeyGenerator(Constant.DEFAULT_KEY_GENERATOR, keyGenerator);
+    // 自动构建SQL 扫描
     AnnotationStatementScanner annotationStatementScanner = new AnnotationStatementScanner.Builder()
-        .configuration(configuration)
-        .basePackages(new String[]{"com.littlenb.mybatisjpa.demo.mapper"})
-        .annotationStatementRegistry(AnnotationStatementRegistry.getDefaultRegistry()).build();
+            .configuration(configuration)
+            .basePackages(new String[]{"com.littlenb.mybatisjpa.demo.mapper"})
+            .annotationStatementRegistry(AnnotationStatementRegistry.getDefaultRegistry()).build();
     annotationStatementScanner.scan();
+    return annotationStatementScanner;
   }
 
 }
